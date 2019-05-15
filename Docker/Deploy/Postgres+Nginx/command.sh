5a2987de4dcacc9a2a314dbd5a2e36034982ea69
Vladimir Belousov <sovmirich@gmail.com>
2019-05-15 17:22:53 +0300

    sdasdsa

--- Docker/Deploy/Postgres+Nginx/command.sh
+++ Docker/Deploy/Postgres+Nginx/command.sh
@@ -1,3 +1,4 @@
 docker network create pg
 docker container run --name pg --network pg --env-file ./Postgres/db.env postgres:latest
 docker container run --name webapp --network pg -p 80:80 --env-file ./Nginx/app.env jfahrer/lb:latest
+
